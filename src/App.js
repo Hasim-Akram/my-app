ba60458f39f3fd2c0bf32a94cf1f8f7b97ce53c0
Hasim-Akram <hasimfci@gmail.com>
2022-07-27 15:28:55 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { hasFormSubmit } from '@testing-library/user-event/dist/utils';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>This is react app</p>
+      
+       <Person name="masum" year="1995"></Person>
+
       </header>
+      
+
+
     </div>
   );
 }
+  
+function Person(props) {
+  return( 
+    <div className='something' style={{border:'2px solid green'}}>
+    <h1>Name:{props.name}</h1>
+    <h1>Year:{props.year}</h1>
+    
+    </div>
+ 
+
+    
+  );
+}
+
+  
 
 export default App;
+
+
+
+
+
+
+
